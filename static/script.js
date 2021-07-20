301bb83f0f056c4ea2ee1f9d0f75703bb52076ef
Nicholas <nicholastang316@gmail.com>
2021-07-20 11:07:15 +0800

    Base addr required field done with 1 bug: When base_addr is set and the operation is regspec dump, the base addr required field does not work correctly after redirect

--- static/script.js
+++ static/script.js
@@ -1,13 +1,19 @@
 var all_fields = ["regspec_file", "reg_offset", "wr_value", "base_addr"];
-function updateHome() {
+function update_required() {
+    var op = document.getElementById("ops");
+    var selected_op = op.options[op.selectedIndex].text;
+
     // Change all input fields back to normal first everytime an operation is selected
     for (var i = 0; i < all_fields.length; i++) {
         document.getElementById(all_fields[i]).required = false;
         change(all_fields[i].concat("_label"), "normal")
     }
 
-    var op = document.getElementById("ops");
-    var selected_op = op.options[op.selectedIndex].text;
+    // If base_addr is not set, makes base_addr a required field when it is not "Dump PCI config space"
+    if (typeof set_base_addr === 'undefined' && selected_op != "Dump PCI config space") {
+        change("base_addr_label", "required");
+        document.getElementById("base_addr").required = true;
+    }
 
     if (selected_op == "Dump PCI config space") {
     } else if (selected_op == "Dump MMIO registers and value in regspec.txt" || selected_op == "Dump MMIO registers and value in regspec.txt on an output file") {
